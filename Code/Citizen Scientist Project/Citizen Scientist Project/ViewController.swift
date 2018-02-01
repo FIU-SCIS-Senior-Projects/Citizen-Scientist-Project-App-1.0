b31f7f8df3a12c1ae3a575b75ac24fa8f26736c0
David Gonzalez <dgonz137@fiu.edu>
2018-01-31 23:37:28 -0500

    Testing commit from xCode

--- Code/Citizen Scientist Project/Citizen Scientist Project/ViewController.swift
+++ Code/Citizen Scientist Project/Citizen Scientist Project/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     // Initial commit
+    //Testing commit from Xcode
     
     override func viewDidLoad() {
         super.viewDidLoad()
