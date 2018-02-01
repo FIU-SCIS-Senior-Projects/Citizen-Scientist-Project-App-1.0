0bbf814d55ae9264276bda8ba74454be20647432
David Gonzalez <dgonz137@fiu.edu>
2018-01-31 23:18:26 -0500

    Initial commit for project Key Bicayne - Citizen Scientist Project

---
+++ Code/Citizen Scientist Project/Citizen Scientist Project/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Citizen Scientist Project
+//
+//  Created by David Gonzalez on 1/31/18.
+//  Copyright © 2018 Key Biscayne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // Initial commit
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
