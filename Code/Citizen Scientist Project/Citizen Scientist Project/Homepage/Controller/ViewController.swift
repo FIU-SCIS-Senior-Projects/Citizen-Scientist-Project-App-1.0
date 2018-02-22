98995d245c6b956cf96a748d12b1b1a1c1171781
Emmanuel Malave <emala003@fiu.edu>
2018-02-22 14:46:46 -0500

    Fixed Navigation Color Issue

--- Code/Citizen Scientist Project/Citizen Scientist Project/Homepage/Controller/ViewController.swift
+++ Code/Citizen Scientist Project/Citizen Scientist Project/Homepage/Controller/ViewController.swift
@@ -30,8 +30,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view, typically from a nib.
+
         collectionView.dataSource = self
         collectionView.delegate = self
         setUpNavBar()
@@ -39,11 +38,13 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func setUpNavBar(){
         self.navigationController?.navigationBar.barStyle = UIBarStyle.blackTranslucent
+        self.navigationController?.navigationBar.isTranslucent = false
         self.navigationController?.navigationBar.barTintColor  = UIColor(red:0.08, green:0.09, blue:0.15, alpha:1.0)
         self.navigationController?.navigationBar.tintColor = UIColor(red:0.60, green:0.66, blue:0.70, alpha:1.0)
         
+        
         let image : UIImage = UIImage(named: "0-csp-app-logo.png")!
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 70, height: 50))
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
         imageView.contentMode = .scaleAspectFit
         imageView.image = image
         self.navigationItem.titleView = imageView
--- Code/Citizen Scientist Project/Citizen Scientist Project/Navigation/Controller/NavigationTableViewController.swift
+++ Code/Citizen Scientist Project/Citizen Scientist Project/Navigation/Controller/NavigationTableViewController.swift
@@ -24,11 +24,12 @@ class NavigationTableViewController: UITableViewController{
     
     func setUpNavBar(){
         self.navigationController?.navigationBar.barStyle = UIBarStyle.blackTranslucent
+        self.navigationController?.navigationBar.isTranslucent = false
         self.navigationController?.navigationBar.barTintColor  = UIColor(red:0.08, green:0.09, blue:0.15, alpha:1.0)
         self.navigationController?.navigationBar.tintColor = UIColor(red:0.60, green:0.66, blue:0.70, alpha:1.0)
         
         let image : UIImage = UIImage(named: "0-csp-app-logo.png")!
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
         imageView.contentMode = .scaleAspectFit
         imageView.image = image
         self.navigationItem.titleView = imageView
