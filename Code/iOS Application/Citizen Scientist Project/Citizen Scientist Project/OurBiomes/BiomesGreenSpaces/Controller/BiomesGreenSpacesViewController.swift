13e129ab708e332cef8acf5006b03c10d8d18cd8
dgonz137 <dgonz137@fiu.edu>
2018-04-13 08:43:57 -0400

    All the Biomes pages were created in storyboard

---
+++ Code/iOS Application/Citizen Scientist Project/Citizen Scientist Project/OurBiomes/BiomesGreenSpaces/Controller/BiomesGreenSpacesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BiomesGreenSpacesViewController.swift
+//  Citizen-Scientist-Project
+//
+//  Created by David Gonzalez on 4/13/18.
+//  Copyright © 2018 Key Biscayne. All rights reserved.
+//
+
+import UIKit
+
+class BiomesGreenSpacesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ReusableHeader.setUpNavBar(navigationController: self.navigationController, navigationItem: self.navigationItem)
+    }
+
+}
---
+++ Code/iOS Application/Citizen Scientist Project/Citizen Scientist Project/OurBiomes/BiomesSeagrasses/Controller/BiomesSeagrassesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BiomesSeagrassesViewController.swift
+//  Citizen-Scientist-Project
+//
+//  Created by David Gonzalez on 4/13/18.
+//  Copyright © 2018 Key Biscayne. All rights reserved.
+//
+
+import UIKit
+
+class BiomesSeagrassesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ReusableHeader.setUpNavBar(navigationController: self.navigationController, navigationItem: self.navigationItem)
+    }
+
+}
